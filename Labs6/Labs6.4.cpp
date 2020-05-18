55fd55bc1e1c6d790f1763b4b3f3cf0fd1c033ba
Drimkore <art.2000m.ab@gmail.com>
2020-05-18 15:17:35 +0500

    Update Labs6.4.cpp
    Labs6.4 - готово

--- Labs6/Labs6.4.cpp
+++ Labs6/Labs6.4.cpp
@@ -1,3 +1,34 @@
 /*4. Сравнить скороость выполнения поиска элемента в неотсортирвоанном и отсортированном массиве
     Результат вывести на экран
 */
+
+#include <iostream>
+#include <ctime>
+#include "header.h"
+#include <time.h>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+    cout.precision (9);
+    int len = 1000;
+    int *array = new int[len];
+    srand(time(NULL));
+    for (int i = 0; i<len; i++) {
+        array[i] = rand() % 2001 -1000;
+    }
+    // Для неотсортированного
+    long double beginFirts = clock();
+    Search(len, 100, array);
+    long double endFirst = clock();
+    cout << "No Sort" << endl << (endFirst - beginFirts)/CLK_TCK << endl;
+
+    recursionQuickSort(array, 0, len-1);
+    // Для отсортированного
+    long double beginSecond = clock();
+    recursionBSearch(array, 0, len-1, 100);
+    long double endSecond = clock();
+    cout << "Sort" << endl << (endSecond - beginSecond)/CLK_TCK << endl;
+    return 0;
+}
