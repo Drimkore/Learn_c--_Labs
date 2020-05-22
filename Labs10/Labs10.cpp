82cdf177836be01b9aa51bfe370a76bdc512ae4e
Drimkore <art.2000m.ab@gmail.com>
2020-05-23 00:37:47 +0500

    Labs19 - Update
    Еще не готово, продолжаю прописывать структуру дочерних классов

--- Labs10/Labs10.cpp
+++ Labs10/Labs10.cpp
@@ -44,14 +44,14 @@ using namespace std;
 
 int main()
 {
-    Matrix2D m2a, m2b;
-    Matrix3D m3a, m3b;
+   Matrix2D m2a, m2b;
+   Matrix3D m3a, m3b;
 
-    cout << m2a << endl;
-    cout << m2b << endl;
+   cout << m2a << endl;
+   cout << m2b << endl;
 
-    cout << m3a << endl;
-    cout << m3b << endl;
+   cout << m3a << endl;
+   cout << m3b << endl;
 
-    return 0;
+   return 0;
 }
--- Labs10/MatrixBase.h
+++ Labs10/MatrixBase.h
@@ -10,32 +10,89 @@ class MatrixBase {
 
     private:
         const unsigned int m_size;
+        int** matrix;
+
     public:
         unsigned const int size() {
             return m_size;
         }
+
         // Возвращает элемент матрицы
-        int element(unsigned int i, unsigned int j) const;
+        virtual int element(unsigned int i, unsigned int j) const = 0;
+
         // Возвращает ссылку на элемент матрицы
-        int& element(unsigned int i, unsigned int j);
+        virtual int& elementLink(unsigned int i, unsigned int j) const = 0;
+
         // Умножение всех элементов на iMult
-        void operator*=(int iMult);
+        void operator*=(int iMult) {
+            for (int i = 0; i < m_size; i++) {
+                for (int j = 0; j < m_size; j++) {
+                    matrix[i][j] *= iMult;
+                }
+            }
+        };
+
         // Сложение матрицы с матрицей iAdd
-        void operator+=(MatrixBase iAdd);
+        void operator+=(MatrixBase iAdd) {
+            for (int i = 0; i < m_size; i++) {
+                for (int j = 0; j < m_size; j++) {
+                    matrix[i][j] += iAdd.element(i,j);
+                }
+            }
+        };
 
 };
 
 ofstream& operator<<(ofstream& stream, const MatrixBase& iMatrix);
 
 class Matrix2D : public MatrixBase {
-    Matrix2D(iSize)
+    private:
+        const unsigned int m_size = 2;
+        int** matrix;
 
+    public:
+        Matrix2D(unsigned int iSize):MatrixBase(iSize) {
+            int num = 1;
+            for (int i = 0; i<iSize; i++) {
+                for (int j = 0; j<iSize; j++) {
+                    matrix[i][j] = num;
+                    num++;
+                }
+            }
+        }
+        ~Matrix2D();
+
+        virtual int element(unsigned int i, unsigned int j) const override {
+            return matrix[i][j];
+        };
+
+        virtual int& elementLink(unsigned int i, unsigned int j) const override {
+            return matrix[i][j];
+        }
 };
 
 class Matrix3D : public MatrixBase {
     private:
-        int*** array[3][3][3];
+        const unsigned int m_size = 3;
+        int** matrix;
+
     public:
+        Matrix3D(unsigned int iSize):MatrixBase(iSize) {
+            int num = 1;
+            for (int i = 0; i<iSize; i++) {
+                for (int j = 0; j<iSize; j++) {
+                    matrix[i][j] = num;
+                    num++;
+                }
+            }
+        }
+        ~Matrix3D();
 
+        virtual int element(unsigned int i, unsigned int j) const override {
+            return matrix[i][j];
+        };
+
+        virtual int& elementLink(unsigned int i, unsigned int j) const override {
+            return matrix[i][j];
+        }
 };
-
